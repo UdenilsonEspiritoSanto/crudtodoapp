6204f191119e4dbf9ac9398db824e655d90c4ab5
UdenilsonEspiritoSanto <149274299+UdenilsonEspiritoSanto@users.noreply.github.com>
2024-03-18 20:06:04 -0300

    todoapp

---
+++ main.py
@@ -0,0 +1,27 @@
+import flet 
+
+from flet import(
+
+Page,
+FloatingActionButton,
+Column,
+Checkbox,
+IconButton,
+OutlinedButton,
+Tabs,
+Text,
+TextField,
+Row,
+UserControl,
+colors,
+icons
+)#importacao dos componentes usados na aplicacao
+
+
+
+
+def main(page:Page):
+    pass
+
+
+flet.app(target=main)
